3452f500b987c1536fabba3dd7be47af91bd3877
Andrey Belchenko <abelchenko@conteq.ru>
2024-07-13 05:42:54 +0300

    1

--- my-app/grab_packages.py
+++ my-app/grab_packages.py
@@ -1,34 +1,24 @@
 import os
 import json
 import shutil
-
-
-def rename_npm_package(project_root, output_root, new_name):
-
-    package_json_path = os.path.join(
-        project_root, "node_modules", "original-package-name", "package.json"
-    )
-
-    new_package_path = os.path.join(output_root, new_name)
-    shutil.copytree(os.path.dirname(package_json_path), new_package_path)
-
-    new_package_json_path = os.path.join(new_package_path, "package.json")
-    with open(new_package_json_path, "r") as f:
-        package_data = json.load(f)
-
-    package_data["name"] = new_name
-
-    with open(new_package_json_path, "w") as f:
-        json.dump(package_data, f, indent=2)
-
-    print(f"Package renamed and copied to {new_package_path}")
+import subprocess
 
 
 source_dir = r"C:\Repos\mygithub\uicomp-test\my-app"
 target_dir = r"C:\Repos\mygithub\uicomp-test\libs"
 
 
-def grab_package(package_name: str, new_package_name):
+def npm_publish(folder_path):
+    os.chdir(folder_path)
+    cmdText = "npm publish"
+    res = os.system(cmdText)
+    if res != 0:
+        raise Exception("Error while executing: " + cmdText)
+    # subprocess.check_call(['cd', folder_path], shell=True)
+    # subprocess.check_call(['npm', 'publish'], shell=True)
+
+
+def grab_package(package_name: str, package_names: list[str]):
     # if new_package_name==None:
     new_dir_name = package_name.replace("\\", "_").replace("/", "_")
     package_path = os.path.join(source_dir, "node_modules", package_name)
@@ -37,13 +27,21 @@ def grab_package(package_name: str, new_package_name):
     shutil.rmtree(new_package_path)
     shutil.copytree(package_path, new_package_path)
 
-    # new_package_json_path = os.path.join(new_package_path, "package.json")
-    # with open(new_package_json_path, "r") as f:
-    #     package_data = json.load(f)
+    new_package_json_path = os.path.join(new_package_path, "package.json")
+    with open(new_package_json_path, "r") as f:
+        package_data = json.load(f)
     # package_data["name"] = new_package_name
 
-    # with open(new_package_json_path, "w") as f:
-    #     json.dump(package_data, f, indent=2)
+    parts = ["dependencies", "devDependencies"]
+    for part in parts:
+        if part in package_data:
+            for name in package_names:
+                deps: dict = package_data[part]
+                if name in deps:
+                    deps.pop(name)
+
+    with open(new_package_json_path, "w") as f:
+        json.dump(package_data, f, indent=2)
 
     npmrc_cont = """registry=http://gl.astu.lan/api/v4/projects/108/packages/npm/
 //gl.astu.lan/api/v4/projects/108/packages/npm/:_authToken=glpat-M1CpxUYSJ3RSuJGXxSEk
@@ -53,10 +51,25 @@ always-auth=true"""
     print(new_package_path)
 
 
-grab_package("devextreme-quill", "duicomp-quill")
-grab_package("devextreme-react", "duicomp-react")
-grab_package("devexpress-diagram", "duicomp-diagram")
-grab_package("devexpress-gantt", "duicomp-gantt")
-grab_package("devextreme", "duicomp")
-grab_package(r"@devexpress\utils", "@duibase/utils")
-grab_package(r"@devextreme\runtime", "@duicomp/runtime")
+def publish_package(package_name: str):
+    new_dir_name = package_name.replace("\\", "_").replace("/", "_")
+    new_package_path = os.path.join(target_dir, new_dir_name)
+    npm_publish(new_package_path)
+    print("Published: " + new_package_path)
+
+
+packages = [
+    "devextreme-quill",
+    "devextreme-react",
+    "devexpress-diagram",
+    "devexpress-gantt",
+    "devextreme",
+    r"@devexpress/utils",
+    r"@devextreme/runtime",
+]
+
+# for item in packages:
+#     grab_package(item, packages)
+
+for item in packages:
+    publish_package(item)
